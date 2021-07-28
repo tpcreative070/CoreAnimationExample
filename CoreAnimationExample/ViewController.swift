50c371c8b4b0ffe2ca3d58dcff6a4e1c9ef2c17c
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-07-28 16:13:53 +0700

    [mater] - The first commit

--- CoreAnimationExample/ViewController.swift
+++ CoreAnimationExample/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let layer = CALayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        layer.backgroundColor = UIColor.systemRed.cgColor
+        layer.frame = CGRect(x: 100, y: 100, width: 120, height: 120)
+        view.layer.addSublayer(layer)
+        
     }
-
-
+    
+    func animateMovement(){
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.fromValue = CGPoint(x: layer.frame.origin.x, y: layer.frame.origin.y)
+        animation.toValue = CGPoint(x: 300, y: 400)
+        animation.duration = 2
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = CACurrentMediaTime()
+        layer.add(animation,forKey : nil)
+    }
+    
 }
 
